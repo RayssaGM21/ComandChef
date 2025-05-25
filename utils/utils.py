c002c9ee1a62a0fb536fb8c42103a5c795db07b8
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-05-25 19:53:44 -0300

    Atualizando utils

---
+++ utils/utils.py
@@ -0,0 +1,85 @@
+import os
+import platform
+from tabulate import tabulate
+from services.chatbot_service import iniciar_chatbot_restaurante
+from services.cliente_service import cadastrar_cliente, listar_clientes
+from services.pedido_service import cadastrar_pedido, listar_fila, retirar_proximo_pedido
+from services.prato_service import cadastrar_prato, listar_pratos, remover_prato
+from dados import pratos, clientes, fila_pedidos
+
+
+def limpar_terminal():
+    sistema = platform.system().lower()
+
+    if sistema == "windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def exibir_opcoes():
+    print("""
+
+░█████╗░░█████╗░███╗░░░███╗███╗░░░███╗░█████╗░███╗░░██╗██████╗░  ░█████╗░██╗░░██╗███████╗███████╗
+██╔══██╗██╔══██╗████╗░████║████╗░████║██╔══██╗████╗░██║██╔══██╗  ██╔══██╗██║░░██║██╔════╝██╔════╝
+██║░░╚═╝██║░░██║██╔████╔██║██╔████╔██║███████║██╔██╗██║██║░░██║  ██║░░╚═╝███████║█████╗░░█████╗░░
+██║░░██╗██║░░██║██║╚██╔╝██║██║╚██╔╝██║██╔══██║██║╚████║██║░░██║  ██║░░██╗██╔══██║██╔══╝░░██╔══╝░░
+╚█████╔╝╚█████╔╝██║░╚═╝░██║██║░╚═╝░██║██║░░██║██║░╚███║██████╔╝  ╚█████╔╝██║░░██║███████╗██║░░░░░
+░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░  ░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░░░░
+""")
+    print("Bem vindo ao ComandChef. O seu restaurante preferido!!\n")
+    menu = [
+        ["1", "Cadastrar Cliente"],
+        ["2", "Cadastrar Prato"],
+        ["3", "Listar Clientes"],
+        ["4", "Listar Pratos"],
+        ["5", "Fazer Pedido"],
+        ["6", "Retirar Próximo Pedido"],
+        ["7", "Remover Prato"],
+        ["8", "Ver fila de pedidos"],
+        ["9", "Converse com o ChatChefBot"],
+        ["q", "Sair"]
+    ]
+
+    cabecalho_menu = ["Opção", "Descrição"]
+    
+    print(tabulate(menu, headers=cabecalho_menu, tablefmt="fancy_grid", numalign="center"))
+
+
+def continuar():
+    input("\nPressione Enter para voltar ao menu principal...")
+    limpar_terminal()
+
+
+def menu_principal():
+    while True:
+        exibir_opcoes()
+        escolha = input("\nDigite a opção desejada: ")
+
+        match escolha:
+            case "1":
+                cadastrar_cliente()
+            case "2":
+                cadastrar_prato()
+            case "3":
+                listar_clientes()
+            case "4":
+                listar_pratos()
+            case "5":
+                cadastrar_pedido()
+            case "6":
+                retirar_proximo_pedido()
+            case "7":
+                listar_pratos()
+                remover_prato()
+            case "8":
+                listar_fila()
+            case "9":
+                iniciar_chatbot_restaurante(pratos, clientes, fila_pedidos)
+            case "q":
+                print("Saindo do sistema...")
+                break
+            case _:
+                print("Opção inválida! Tente novamente!")
+
+        continuar()
